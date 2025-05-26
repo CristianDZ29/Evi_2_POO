da803e6604862e01f2a46f7ee399ace61cd2d740
CristianRen <cristian-0405ra@hotmail.com>
2025-05-25 19:13:04 -0500

    Se crea la clase Gestión Personas, se importa la libreria java.util.Scanner, y se inicia a crear los métodos

--- src/GestionPersonas.java
+++ src/GestionPersonas.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class GestionPersonas {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.println("=== SISTEMA DE GESTIÓN DE PERSONAS ===");
+        System.out.println("Demostración de Programación Orientada a Objetos en Java\n");
+
+
+        Persona[] personas = null;
+
+        mostrarMenuPrincipal(personas);
+
+        scanner.close();
+    }
+
+}
