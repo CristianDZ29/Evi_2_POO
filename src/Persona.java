200e05b7435c6ef58377715bbf4125e27fb0c9c5
CristianRen <cristian-0405ra@hotmail.com>
2025-05-25 19:07:50 -0500

    Se inicia el proyecto

---
+++ src/Persona.java
@@ -0,0 +1,14 @@
+public class Persona{
+    private String nombre;
+    private String apellido;
+    private String genero;
+    private int edad;
+
+    public Persona(String nombre, String apellido, String genero, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.edad = edad;
+    }
+
+}
